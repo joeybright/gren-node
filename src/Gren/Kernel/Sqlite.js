b4adb9da6874e109a5115701c4e5235aeb07a9c8
Joey Bright <hello@joeybright.com>
2026-08-17 20:14:53 -0700

    Implement `Sqlite.backup`
    Implementation and integration tests for the `backup` SQLite function

--- src/Gren/Kernel/Sqlite.js
+++ src/Gren/Kernel/Sqlite.js
@@ -52,6 +52,27 @@ var _Sqlite_close = function (db) {
   });
 };
 
+var _Sqlite_backup = F3(function (destination, pages, db) {
+  return __Scheduler_binding(function (callback) {
+    try {
+      sqlite
+        .backup(db, destination, {
+          source: "main",
+          target: "main",
+          rate: pages,
+        })
+        .then(function (res) {
+          callback(__Scheduler_succeed({}));
+        })
+        .catch(function (e) {
+          callback(_Sqlite_constructError(e));
+        });
+    } catch (e) {
+      callback(_Sqlite_constructError(e));
+    }
+  });
+});
+
 var _Sqlite_foldl = F4(function (query, db, func, acc) {
   return __Scheduler_binding(function (callback) {
     try {
